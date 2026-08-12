f00b186370cec611a362b43eecb13d85affb3eed
Vinit Mishra <mishravinit923@gmail.com>
2026-08-12 21:47:02 +0530

    Delete START-BIKER-STATION.bat

--- START-BIKER-STATION.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-where py >nul 2>nul && (start "" "http://localhost:5500" & py -m http.server 5500 & pause & exit /b)
-where python >nul 2>nul && (start "" "http://localhost:5500" & python -m http.server 5500 & pause & exit /b)
-echo Python not found.
-pause
